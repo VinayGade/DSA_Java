a769cf384c693f93cf9f68bbc4ce2fe2e89a57de
VinayGade <vinay.gaade@gmail.com>
2023-03-17 12:27:20 +0530

    Find occurrence of each element in unsorted array using Number Frequency Map

--- src/recursion/CountFrequencies.java
+++ src/recursion/CountFrequencies.java
@@ -1,6 +1,9 @@
 package recursion;
 
 import java.util.Arrays;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 /*
 * Find occurrence of each element in unsorted array
@@ -31,32 +34,39 @@ public class CountFrequencies {
         }
     }
 
-    private void frequencies(int [] nums){
+    // Map<number, frequency> ... efficient solution,  Time Complexity : O(N)
+    public Map<Integer, Integer> frequencyMap(int [] arr){
 
-        int count = 0;
-        int n = nums.length;
-        int[] freuencyList = new int[n];
+        Map<Integer, Integer> numberFrequencyMap= new LinkedHashMap<>();
 
-        for(int i=0; i<n; i++){
+        for(int i=0; i<arr.length; i++){
 
-            count = 1;
-            for(int j=i+1; j<n; j++){
-
-                if(nums[i] == nums[j]){
-
-                    count++;
-                    freuencyList[j] = 0;
-                }
-            }
-
-            if(freuencyList[i] != 0)
-                freuencyList[i] = count;
+            if(numberFrequencyMap.containsKey(arr[i]))
+                numberFrequencyMap.put(arr[i], numberFrequencyMap.get(arr[i]) + 1);
+            else
+                numberFrequencyMap.put(arr[i], 1);
         }
+        return numberFrequencyMap;
+    }
 
-        System.out.println("The elements and their occurences are");
-        for(int i = 0; i<nums.length; i++)
-            if(freuencyList[i]>=1)
-                System.out.println("Element="+nums[i]+", Frequency="+freuencyList[i]);
+    public void iterateUsingStreamAPI(Map<Integer, Integer> map) {
+        map.entrySet()
+                .stream()
+                .forEach(e -> System.out.println("Number="+e.getKey() + ", Frequency=" + e.getValue()));
+    }
+
+    public void iterateUsingIteratorAndEntry(Map<Integer, Integer> map) {
+        Iterator<Map.Entry<Integer, Integer>> iterator = map.entrySet().iterator();
+        while (iterator.hasNext()) {
+            Map.Entry<Integer, Integer> entry = iterator.next();
+            System.out.println("Number="+entry.getKey() + ", Frequency=" + entry.getValue());
+        }
+    }
+
+    public void iterateUsingEntrySet(Map<Integer, Integer> map) {
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            System.out.println("[Number: "+entry.getKey() + ", Frequency:" + entry.getValue()+"]");
+        }
     }
 
     public static void main(String[] args) {
@@ -65,5 +75,20 @@ public class CountFrequencies {
         CountFrequencies frequencies = new CountFrequencies();
         frequencies.countFrequencies(arr);
 
+        System.out.println("\nNumber frequency map : ");
+        Map<Integer, Integer> numFrequencyMap = frequencies.frequencyMap(arr);
+
+        numFrequencyMap.forEach(
+                (k,v) -> System.out.println("Number = "+k+", Frequency = "+v)
+        );
+
+        System.out.println("\nIterating Number frequency map using stream API: ");
+        frequencies.iterateUsingStreamAPI(numFrequencyMap);
+
+        System.out.println("\nNumber frequency map using Iterator: ");
+        frequencies.iterateUsingIteratorAndEntry(numFrequencyMap);
+
+        System.out.println("\nNumber frequency map using EntrySet: ");
+        frequencies.iterateUsingEntrySet(numFrequencyMap);
     }
 }
