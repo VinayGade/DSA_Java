86e2aa4b5f2eb0e1cc8154a12a3dfbe124cb8c62
VinayGade <vinay.gaade@gmail.com>
2023-07-20 21:56:30 +0530

    Dual pivot quick sort: best sorting worst case O(N) best case O(lg N)

---
+++ src/sort/DualPivotQuickSort.java
@@ -0,0 +1,98 @@
+package sort;
+
+public class DualPivotQuickSort {
+
+    // Java program to implement
+    // dual pivot QuickSort
+
+    public static void dualPivotQuickSort(int[] arr,
+                                   int low, int high) {
+        if (low < high) {
+
+            // piv[] stores left pivot and right pivot.
+            // piv[0] means left pivot and
+            // piv[1] means right pivot
+            int[] piv;
+            piv = partition(arr, low, high);
+
+            dualPivotQuickSort(arr, low, piv[0] - 1);
+            dualPivotQuickSort(arr, piv[0] + 1, piv[1] - 1);
+            dualPivotQuickSort(arr, piv[1] + 1, high);
+        }
+    }
+
+    static int[] partition(int[] arr, int low, int high) {
+        if (arr[low] > arr[high])
+            swap(arr, low, high);
+
+        // p is the left pivot, and q
+        // is the right pivot.
+        int j = low + 1;
+        int g = high - 1, k = low + 1,
+                p = arr[low], q = arr[high];
+
+        while (k <= g) {
+
+            // If elements are less than the left pivot
+            if (arr[k] < p) {
+                swap(arr, k, j);
+                j++;
+            }
+
+            // If elements are greater than or equal
+            // to the right pivot
+            else if (arr[k] >= q) {
+                while (arr[g] > q && k < g)
+                    g--;
+
+                swap(arr, k, g);
+                g--;
+
+                if (arr[k] < p) {
+                    swap(arr, k, j);
+                    j++;
+                }
+            }
+            k++;
+        }
+        j--;
+        g++;
+
+        // Bring pivots to their appropriate positions.
+        swap(arr, low, j);
+        swap(arr, high, g);
+
+        // Returning the indices of the pivots
+        // because we cannot return two elements
+        // from a function, we do that using an array.
+        return new int[]{j, g};
+    }
+
+    static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    static void display(int[] arr, int n){
+        for (int i = 0; i < n; i++)
+            System.out.print(arr[i] + " ");
+    }
+    // Driver code
+    public static void main(String[] args) {
+
+        int[] arr = {24, 8, 42, 75, 29, 77, 38, 57, 100, 200, 90, 60, 5, 1, 80, 50, 10};
+
+        int n = arr.length;
+
+        System.out.print("original array: ");
+        display(arr, n);
+
+        dualPivotQuickSort(arr, 0, arr.length-1);
+
+        System.out.print("\nSorted array: ");
+
+        display(arr, n);
+        System.out.println();
+    }
+}
